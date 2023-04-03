645065aa22d3c8ed43ff1a5da91e1c6cf549db80
Zaki Orya <zakioria@gmail.com>
2023-04-04 01:58:15 +0300

    z-20 to dropdown

--- src/components/dashboard/boardOptionsDropdown.tsx
+++ src/components/dashboard/boardOptionsDropdown.tsx
@@ -70,7 +70,7 @@ export default function BoardOptionsDropdown({ boardId }: Props) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute right-0 z-20 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div>
             <Menu.Item>
 
